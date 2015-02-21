69bcef9b876e89ce91b81c330451cd8aa542646f
s54192 <s54192@beuth-hochschule.de>
2015-02-21 21:58:59 +0100

    added listHashMap()

--- src/mainFunctions/xvmFile.java
+++ src/mainFunctions/xvmFile.java
@@ -4,25 +4,37 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Set;
 
 public class xvmFile {
 	
-	public HashMap fileContents = new HashMap();
+	public HashMap<String,Object> fileContents = new HashMap<String,Object>();
 	public ArrayList<File> fileList = new ArrayList<File>();
+	
+	private Set xvmSet;
+	private Iterator iterHashMap;
+	private Map.Entry xvmHashMapEntry;
 	
 	public xvmFile(String path){
 		File file = new File(path);
-		
-		
-		
 		Collections.addAll(fileList, file.listFiles());
+		
 		
 		for (int i = 0; i < fileList.size(); i++){
 			fileContents.put(fileList.get(i).getName(),i);
-			
-			
+		}	
+	}
+	
+	public void listHashMap(){
+		this.xvmSet = fileContents.entrySet();
+		this.iterHashMap = xvmSet.iterator();
+		
+		while(iterHashMap.hasNext()){
+			xvmHashMapEntry = (Map.Entry)iterHashMap.next();
+			System.out.print(xvmHashMapEntry.getKey()+": ");
+			System.out.println(xvmHashMapEntry.getValue());
 		}
-		
-		
 	}
 }
---
+++ src/xvmMain.java
@@ -0,0 +1,12 @@
+import mainFunctions.xvmFile;
+
+public class xvmMain {
+
+	public static void main(String[] args) {
+		xvmFile file = new xvmFile("C:\\Games\\World_of_Tanks\\res_mods\\configs\\xvm\\default\\");
+		file.listHashMap();
+		
+
+	}
+
+}
