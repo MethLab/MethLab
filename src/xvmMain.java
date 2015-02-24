7bde5840d05b81ee3577b9c5a87d168611f5ba0d
s54192 <s54192@beuth-hochschule.de>
2015-02-24 23:35:12 +0100

    .

--- src/xvmMain.java
+++ src/xvmMain.java
@@ -1,10 +1,17 @@
+import java.io.IOException;
+
 import mainFunctions.xvmFile;
+import mainFunctions.xvmTag;
 
 public class xvmMain {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		xvmFile file = new xvmFile("C:\\Games\\World_of_Tanks\\res_mods\\configs\\xvm\\default\\");
-		file.listHashMap();
+		xvmTag tag = new xvmTag("\"\\w+\"[^,]\\s");
+//		file.listHashMap();
+		tag.getTags(file.fileContents.get("hangar.xc"));
+		System.out.println(tag.tagList);
+		
 		
 
 	}
